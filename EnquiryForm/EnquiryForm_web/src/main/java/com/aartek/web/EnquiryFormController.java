908d058bf3d1ae1a9347e0f84e6fefeb9601cd77
Ankit Sakatpuriya <Ankit Sakatpuriya@ANKITSAKATPURIYA>
2018-08-14 16:59:12 +0530

    Delete

--- EnquiryForm/EnquiryForm_web/src/main/java/com/aartek/web/EnquiryFormController.java
+++ EnquiryForm/EnquiryForm_web/src/main/java/com/aartek/web/EnquiryFormController.java
@@ -11,24 +11,5 @@ public class EnquiryFormController {
 	@Autowired
 	EnquiryFormService efs;
 
-	// priya
-	// Ankit ki commit
-	// Prashant commit 
-		
-		{
-
-		System.out.println("p1");
-		System.out.println("Ankit sakatpuriya");
-		System.out.println("356748541");
-		System.out.println("Prashant");
-		System.out.println("p1");
-		System.out.println("Prshant");
-		System.out.println("Priya Choti ");
-		System.out.println("8527418965324104106523052041020");
-		System.out.println("Prshant");
-		System.out.println("Prshant");
-		}		
-		static{
-			System.out.println("We are just testing ");
-		}
 }
+		
